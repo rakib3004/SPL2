3cbef0443de9e96aa8d1c0440063667798f049c9
rakib3004 <bsse1129@iit.du.ac.bd>
2022-02-19 00:31:22 +0600

    add multiple video to text converter

--- YouTubeToPlainText.py
+++ YouTubeToPlainText.py
@@ -2,22 +2,29 @@ from youtube_transcript_api import YouTubeTranscriptApi as yta
 
 import re
 
-video_id = 'SUOpsaDV1W4'
+video_group = ['jUuqBZwwkQw','vAoB4VbhRzM','xT8oP0wy-A0','x7X9w_GIm1s']
+#'l9AzO1FMgM8','ravLFzIguCM'
 
-data = yta.get_transcript(video_id)
+#firebase, lua, kotline, python, java, c sharp
+lanID=1
+for video_id in video_group:
 
-transcript = ''
-for value in data:
-    for key, val in value.items():
-        if key=='text':
-            transcript+=val
+    data = yta.get_transcript(video_id)
+
+    transcript = ''
+    for value in data:
+        for key, val in value.items():
+            if key=='text':
+                transcript+=val+"\n"
 
 
-expression_info = transcript.splitlines()
-final_transcript = " ".join("\n")
-final_transcript = " ".join(expression_info)
+    expression_info = transcript.splitlines()
+    print(expression_info)
+    final_transcript = " ".join(expression_info)
 
 
-file = open("SPL_ONE.txt","w")
-file.write(final_transcript)
-file.close()
+    language = "Language"+str(lanID)+".txt"
+    lanID=lanID+1
+    file = open(language,"w")
+    file.write(final_transcript)
+    file.close()
