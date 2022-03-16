0e87b57062cbfa649c13517655021b3a0d3a7645
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-16 19:37:55 +0600

    clear code

--- TextClassificationImplementation.py
+++ TextClassificationImplementation.py
@@ -16,24 +16,25 @@ from nltk.stem.snowball import SnowballStemmer
 twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
 
 
-#print('20 Train data',twenty_train.target_names)
+print('20 Train data',twenty_train.target_names)
+
+print("\n".join(twenty_train.data[0].split("\n")[:3])) #prints first line of the first data file
 
 
-
-#print("\n".join(twenty_train.data[0].split("\n")[:3]))
+print(len(twenty_train.data))
 
 
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(twenty_train.data)
-#print('Train Set Count: ',X_train_counts.shape)
+print('Train Set Count: ',X_train_counts.shape)
 
 
 
 
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-#print('Term Frequency Data: \n',X_train_tfidf)
-#print('Train Text Frequency: ',X_train_tfidf.shape)
+print('Term Frequency Data: \n',X_train_tfidf)
+print('Train Text Frequency: ',X_train_tfidf.shape)
 
 
 
@@ -56,18 +57,18 @@ gs_clf = gs_clf.fit(twenty_train.data, twenty_train.target)
 predicted_gs_clf=gs_clf.predict(twenty_test.data)
 
 
-##print('Grid Search Prediction: ',predicted_gs_clf)
-##print('Testing  value: ',twenty_test.target)
+#print('Grid Search Prediction: ',predicted_gs_clf)
+#print('Testing  value: ',twenty_test.target)
 
-##print('Grid Search Accuracy dataset: ', predicted_gs_clf==twenty_test.target)
+#print('Grid Search Accuracy dataset: ', predicted_gs_clf==twenty_test.target)
              
 
 
 
 
-##print('Grid Search Accuracy %: ', np.mean(predicted_gs_clf==twenty_test.target))
-
+#print('Grid Search Accuracy %: ', np.mean(predicted_gs_clf==twenty_test.target))
 '''
+
 print('The best score is: ',gs_clf.best_score_)
 print('The best params is: ',gs_clf.best_params_)
 
