9d792a50b04063b8d222f7f461880bfbc96f6ad5
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-17 17:26:53 +0600

    clean data

--- TrainData.py
+++ TrainData.py
@@ -26,7 +26,7 @@ text_clf = text_clf.fit(twenty_train.data, twenty_train.target)
 twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
 
 
-deployment_set= open("ConvertedText/Language2.txt","r")
+deployment_set= open("ConvertedText/Language6.txt","r")
 
 deployment_data = deployment_set.read()
 
@@ -35,12 +35,12 @@ deployment_list = list(deployment_data)
 predicted = text_clf.predict(twenty_test.data)
 
 
-news='python a high-level interpreted'
+news='We are continuing our series of animated historical videos on the modern wars with the Six-Day War of 1967, also known as the Third Arab–Israeli War during which Israel fought the alliance of Egypt, Syria, Jordan, Iraq. This conflict entered history as short, but decisive, as it took less than a '
 
 computationalNews=[news]
 
 
-tagPrediction=text_clf.predict(deployment_list)
+tagPrediction=text_clf.predict(computationalNews)
 print(twenty_train.target_names[tagPrediction[0]])
 
 tagList = open("TagList.txt", "a")
