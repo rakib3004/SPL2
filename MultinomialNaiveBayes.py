74ddf382a3fcce18fb8b7ed5bbe89d3061a6aea2
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-29 13:49:56 +0600

    add new content

--- MultinomialNaiveBayes.py
+++ MultinomialNaiveBayes.py
@@ -10,7 +10,8 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from nltk.stem.snowball import SnowballStemmer
-
+import glob
+import os
 
 
 
@@ -26,31 +27,20 @@ text_clf = text_clf.fit(twenty_train.data, twenty_train.target)
 twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
 
 
-deployment_set= open("ConvertedText/Language3.txt","r")
+topicSource='ConvertedText/*'
 
-deployment_data = deployment_set.read()
+for deployment_set in glob.glob(topicSource):
 
-deployment_list = [deployment_data]
+    deployment_list = open(deployment_set)
+    deployment_data = deployment_list.read()
+    deployment_list = [deployment_data]
 
-predicted = text_clf.predict(twenty_test.data)
+    fileName=os.path.basename(deployment_set)
+    deploymentName=os.path.splitext(fileName)[0]
 
-
-
-tagPrediction=text_clf.predict(deployment_list)
-print(twenty_train.target_names[tagPrediction[0]])
-tagElement = twenty_train.target_names[tagPrediction[0]].split('.')
-
-print(tagElement[:])
-'''
-tagList = open("TagList.txt", "w")
-
-tagList.write(str(twenty_train.target_names))
-
-tagList.close()
-
-'''
-
-
+    tagPrediction=text_clf.predict(deployment_list)
+    tagElement = twenty_train.target_names[tagPrediction[0]].split('.')
+    print(deploymentName, tagElement[:])
 
 
 
