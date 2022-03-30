c0b8d322e5101cd188292935ef48411477aab6dd
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-30 13:31:55 +0600

    add call information about youtube

--- YouTubeToPlainText.py
+++ YouTubeToPlainText.py
@@ -1,20 +1,14 @@
 from youtube_transcript_api import YouTubeTranscriptApi as yta
 
-
 import re
 
-
-
-my_file = open("youtube_url.txt", "r")
+my_file = open("test_blog.txt", "r")
 
 data = my_file.read()
-
-
 
 video_group = data.split("\n")
 print(video_group)
 my_file.close()
-
 
 lanID=1
 for video_id in video_group:
@@ -27,10 +21,7 @@ for video_id in video_group:
             if key=='text':
                 transcript+=val+"\n"
 
-
-
-
-    language = "ConvertedText/Language"+str(lanID)+".txt"
+    language = "BlogText/Language"+str(lanID)+".txt"
     lanID=lanID+1
     file = open(language,"w")
     file.write(transcript)
