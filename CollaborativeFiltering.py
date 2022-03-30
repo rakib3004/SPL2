a873da12848217c54c5b0f629816548201671f43
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-30 15:55:41 +0600

    everything

--- CollaborativeFiltering.py
+++ CollaborativeFiltering.py
@@ -4,8 +4,7 @@ from scipy.sparse import csc_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
 import json
-import gzip
-import io
+
 
 def extractPath(innie):
     iggy = str(innie)
@@ -15,16 +14,11 @@ def extractPath(innie):
     iggy = iggy.replace(getridofmetoo, "")
     return iggy
 
-'''with open("Amazon_Instant_Video_5.json", "r", encoding='utf-8-sig') as f:
-    data = f.read()'''
-    
 
 
-'''data = open('Amazon_Instant_Video_5.json')
-data = open('Amazon_Instant_Video_5.json', "r+", encoding='utf-8-sig')'''
 data = open('Amazon_Instant_Video_5.json')
-data = extractPath(data)
-df = json.loads(data)
+data2 = extractPath(data)
+df = json.loads(data2)
 
 
 avg_num_reviews = df.groupby('reviewerID')['asin'].count()
