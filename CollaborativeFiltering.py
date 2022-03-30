24bab6dc035543400f9edd263821a80ff81fab47
rakib3004 <bsse1129@iit.du.ac.bd>
2022-03-30 12:32:37 +0600

    add blog preview

--- CollaborativeFiltering.py
+++ CollaborativeFiltering.py
@@ -3,19 +3,27 @@ import numpy as np
 from scipy.sparse import csc_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-
 import json
 import gzip
 import io
 
+def extractPath(innie):
+    iggy = str(innie)
+    getridofme ="<_io.TextIOWrapper name='"
+    getridofmetoo ="' mode='r' encoding='UTF-8'>"
+    iggy = iggy.replace(getridofme, "")
+    iggy = iggy.replace(getridofmetoo, "")
+    return iggy
 
-with open("Amazon_Instant_Video_5.json", "r", encoding='utf-8-sig') as f:
-    data = f.read()
-    df = json.loads(data)
-    print(df.info())
+'''with open("Amazon_Instant_Video_5.json", "r", encoding='utf-8-sig') as f:
+    data = f.read()'''
+    
 
 
 '''data = open('Amazon_Instant_Video_5.json')
 data = open('Amazon_Instant_Video_5.json', "r+", encoding='utf-8-sig')'''
- 
+data = open('Amazon_Instant_Video_5.json')
+data = extractPath(data)
+df = json.loads(data)
+
+print(df.info())
