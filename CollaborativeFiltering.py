78439229205c4cea9f53550c2b634145b657a698
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-01 15:44:54 +0600

    unable to resolve json file

--- CollaborativeFiltering.py
+++ CollaborativeFiltering.py
@@ -1,34 +1,25 @@
-import pandas as pd
+'''import pandas as pd
 import numpy as np
-from scipy.sparse import csc_matrix
+from scipy.sparse import csc_matrix'''
 import json
 
-f = open('BlogRating.json')
+'''f = open('BlogRating.json')
 
-data = json.load(f)
+data = json.load(f)'''
 
+with open('BlogRating.json', 'r') as myfile:
+    data=myfile.read()
 
-def extractPath(innie):
-    iggy = str(innie)
-    getridofme ="<_io.TextIOWrapper name='"
-    getridofmetoo ="' mode='r' encoding='UTF-8'>"
-    iggy = iggy.replace(getridofme, "")
-    iggy = iggy.replace(getridofmetoo, "")
-    return iggy
+# parse file
+df = json.loads(data)
+
 
 
 '''
-data = open('Amazon_Instant_Video_5.json')
-data2 = extractPath(data)
-df = json.loads(data2)
-
-parsed_json = (json.loads(json_data))
-print(json.dumps(parsed_json, indent=4, sort_keys=True))
 
 
-avg_num_reviews = df.groupby('reviewerID')['asin'].count()
+avg_num_reviews = df.groupby('reviewerID').count()
 mean_rating_of_user = df.groupby('reviewerID').apply(lambda x: x['overall'].mean())
 
 print(avg_num_reviews,mean_rating_of_user)
-
 '''
