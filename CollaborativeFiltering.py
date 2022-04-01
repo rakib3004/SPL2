eebbda2c2e0439b4bd003f946746d1a1686268d0
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-01 12:38:54 +0600

    huge data

--- CollaborativeFiltering.py
+++ CollaborativeFiltering.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
 from scipy.sparse import csc_matrix
-import seaborn as sns
-import matplotlib.pyplot as plt
 import json
+
+with open('Amazon_Instant_Video_5.json', 'r') as json_file:
+	json_load = json.load(json_file)
+
+parsed_json = (json.loads(json_data))
+print(json.dumps(parsed_json, indent=4, sort_keys=True))
 
 
 def extractPath(innie):
@@ -15,10 +19,13 @@ def extractPath(innie):
     return iggy
 
 
-
+'''
 data = open('Amazon_Instant_Video_5.json')
 data2 = extractPath(data)
 df = json.loads(data2)
+
+parsed_json = (json.loads(json_data))
+print(json.dumps(parsed_json, indent=4, sort_keys=True))
 
 
 avg_num_reviews = df.groupby('reviewerID')['asin'].count()
@@ -26,3 +33,4 @@ mean_rating_of_user = df.groupby('reviewerID').apply(lambda x: x['overall'].mean
 
 print(avg_num_reviews,mean_rating_of_user)
 
+'''
