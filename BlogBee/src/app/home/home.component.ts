75c65b9428ed8d52781ad762e5710e0af9707617
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-07 11:52:11 +0600

    reformate blog brr  module

--- BlogBee/src/app/home/home.component.ts
+++ BlogBee/src/app/home/home.component.ts
@@ -1,23 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
+  showBlog(path: String) {}
 
-  showBlog(path: String){
-    
-  }
-
-  
-  
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- BlogBee/src/app/navbar/navbar.component.ts
+++ BlogBee/src/app/navbar/navbar.component.ts
@@ -3,21 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  click = 0;
 
-  click=0;
-
-  menuButtonClickEvent(){
-     if(this.click==0) this.click=1;
-     else this.click=0;
+  menuButtonClickEvent() {
+    if (this.click == 0) this.click = 1;
+    else this.click = 0;
   }
 
-  
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
