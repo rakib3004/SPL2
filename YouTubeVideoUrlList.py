9e0e2b22a3f78fa509020a30f49fd8fbed1df8b0
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 19:14:59 +0600

    listed topics

--- YouTubeVideoUrlList.py
+++ YouTubeVideoUrlList.py
@@ -1,15 +1,22 @@
+import random
 youTubeVideoUrlListPlainText = open("YouTubePlayList.txt", encoding="utf8")
 
 youTubeVideoUrlListFile=youTubeVideoUrlListPlainText.readlines()
 
+
+youTubeVideoUrlListVideoTopic = open("YouTubeVideoTopic.txt", encoding="utf8")
+
+youTubeVideoTopic=youTubeVideoUrlListVideoTopic.readlines()
 
 videoDataSteam=1
 videoID=1
 
 for videoUrl in youTubeVideoUrlListFile:
     if(videoDataSteam%2==1):
-        print(videoUrl, end='')
+        #print(videoUrl, end='')
         videoID = videoID + 1
+        topicID=random.randint(0,17)
+        print(youTubeVideoTopic[topicID], end='')
 
     videoDataSteam=videoDataSteam+1
 
