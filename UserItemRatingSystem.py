29e5cb1cbb04d030dce5fa47ef705f24e7eb2985
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-14 17:47:10 +0600

    add new data set

--- UserItemRatingSystem.py
+++ UserItemRatingSystem.py
@@ -3,13 +3,13 @@ from datetime import datetime
 
 
 dataFileName=open("rating.data","w")
-for i in range(70000):
+for i in range(7500):
     dt = datetime.now()
 
     timestamp = datetime.timestamp(dt)
     timestamp=int(timestamp)
-    userID=random.randrange(1,632)
-    itemID=random.randrange(1,160)
+    userID=random.randrange(1,300)
+    itemID=random.randrange(1,46)
     rating=random.randrange(1,5)
     print(userID,itemID,rating,timestamp)
     dataFileName.write(str(userID)+"\t"+str(itemID)+"\t"+str(rating)+"\t"+str(timestamp)+"\n")
