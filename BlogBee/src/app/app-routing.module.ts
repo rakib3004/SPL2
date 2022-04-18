486ccd834bbda3a98c97322fe118c2579bf59b81
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-18 12:07:27 +0600

    put fav data list

--- BlogBee/src/app/app-routing.module.ts
+++ BlogBee/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { SignupComponent } from './signup/signup.component';
 import { BlogViewComponent } from './blog-view/blog-view.component';
 import { HomeComponent } from './home/home.component';
+import { FavouriteListComponent } from './favourite-list/favourite-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'signin', component: SignInComponent },
   { path: 'navbar', component: NavbarComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'blogView', component: BlogViewComponent }
+  { path: 'blogView', component: BlogViewComponent },
+  { path: 'favList', component: FavouriteListComponent },
 ];
 
 @NgModule({
--- BlogBee/src/app/favourite-list/favourite-list.component.ts
+++ BlogBee/src/app/favourite-list/favourite-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Video } from '../video';
+import { VideoService } from '../video.service';
 
 @Component({
   selector: 'app-favourite-list',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavouriteListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private videoService: VideoService, private router: Router) {}
+  videos: Video[] = [];
+  favVideoOne:Video | undefined;
+  favVideoOne:Video | undefined;
+  favVideoOne:Video | undefined;
+  favVideoOne:Video | undefined;
+  favVideoOne:Video | undefined;
+  favVideoOne:Video | undefined;
+
 
   ngOnInit(): void {
+    this.videos = this.videoService.getVideos();
+    favVideoOne=this.videos[0];
+    favVideoTwo=this.videos[1];
+    favVideoThree=this.videos[2];
+    favVideoFour=this.videos[3];
+    favVideoFive=this.videos[4];
+    favVideoSix=this.videos[5];
   }
+ 
 
 }
