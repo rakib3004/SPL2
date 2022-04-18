e86d79b3aa15b5aef03eee5b4f3b03b9b5d70ba8
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-18 19:19:46 +0600

    rest api conflict

--- BlogBee/src/app/video.service.ts
+++ BlogBee/src/app/video.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Video} from './video';
-import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
-import { catchError, map, Observable, throwError } from 'rxjs';
+// import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+// import { catchError, map, Observable, throwError } from 'rxjs';
 
 
  
@@ -47,60 +47,60 @@ export class VideoService {
     return this.videos;
   }
 
-  REST_API: string = 'http://localhost:3000/';
+  // REST_API: string = 'http://localhost:3000/';
 
-  constructor(private httpClient: HttpClient) { }
+  // constructor(private httpClient: HttpClient) { }
 
 
-  httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
-  // Add
-  AddUser(data: Video): Observable<any> {
-    let API_URL = `${this.REST_API}/add-video`;
-    return this.httpClient.post(API_URL, data)
-      .pipe(
-        catchError(this.handleError)
-      )
-  }
-  // Get all objects
-  GetUsers() {
-    return this.httpClient.get(`${this.REST_API}/videos`);
-  }
-  // Get single object
-  GetUser(id:any): Observable<any> {
-    let API_URL = `${this.REST_API}/video/${id}`;
-    return this.httpClient.get(API_URL, { headers: this.httpHeaders })
-      .pipe(map((res: any) => {
-          return res || {}
-        }),
-        catchError(this.handleError)
-      )
-  }
-  // Update
-  updateUser(id:any, data:any): Observable<any> {
-    let API_URL = `${this.REST_API}/update-video/${id}`;
-    return this.httpClient.put(API_URL, data, { headers: this.httpHeaders })
-      .pipe(
-        catchError(this.handleError)
-      )
-  }
-  // Delete
-  deleteUser(id:any): Observable<any> {
-    let API_URL = `${this.REST_API}/delete-video/${id}`;
-    return this.httpClient.delete(API_URL, { headers: this.httpHeaders}).pipe(
-        catchError(this.handleError)
-      )
-  }
-  // Error 
-  handleError(error: HttpErrorResponse) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      // Handle client error
-      errorMessage = error.error.message;
-    } else {
-      // Handle server error
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-    }
-    console.log(errorMessage);
-    return throwError(errorMessage);
-  }
+  // httpHeaders = new HttpHeaders().set('Content-Type', 'application/json');
+  // // Add
+  // AddUser(data: Video): Observable<any> {
+  //   let API_URL = `${this.REST_API}/add-video`;
+  //   return this.httpClient.post(API_URL, data)
+  //     .pipe(
+  //       catchError(this.handleError)
+  //     )
+  // }
+  // // Get all objects
+  // GetUsers() {
+  //   return this.httpClient.get(`${this.REST_API}/videos`);
+  // }
+  // // Get single object
+  // GetUser(id:any): Observable<any> {
+  //   let API_URL = `${this.REST_API}/video/${id}`;
+  //   return this.httpClient.get(API_URL, { headers: this.httpHeaders })
+  //     .pipe(map((res: any) => {
+  //         return res || {}
+  //       }),
+  //       catchError(this.handleError)
+  //     )
+  // }
+  // // Update
+  // updateUser(id:any, data:any): Observable<any> {
+  //   let API_URL = `${this.REST_API}/update-video/${id}`;
+  //   return this.httpClient.put(API_URL, data, { headers: this.httpHeaders })
+  //     .pipe(
+  //       catchError(this.handleError)
+  //     )
+  // }
+  // // Delete
+  // deleteUser(id:any): Observable<any> {
+  //   let API_URL = `${this.REST_API}/delete-video/${id}`;
+  //   return this.httpClient.delete(API_URL, { headers: this.httpHeaders}).pipe(
+  //       catchError(this.handleError)
+  //     )
+  // }
+  // // Error 
+  // handleError(error: HttpErrorResponse) {
+  //   let errorMessage = '';
+  //   if (error.error instanceof ErrorEvent) {
+  //     // Handle client error
+  //     errorMessage = error.error.message;
+  //   } else {
+  //     // Handle server error
+  //     errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+  //   }
+  //   console.log(errorMessage);
+  //   return throwError(errorMessage);
+  // }
 }
