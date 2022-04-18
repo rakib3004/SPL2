a42f63ccb02e191bf0c8400bcb9ce81d9301904e
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-18 14:07:49 +0600

    add everything

--- MERN/app.js
+++ MERN/app.js
@@ -1,46 +1,135 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyparser = require('body-parser');
-var cors = require('cors');
-var path = require('path');
+// var express = require('express');
+// var mongoose = require('mongoose');
+// var bodyparser = require('body-parser');
+// var cors = require('cors');
+// var path = require('path');
 
 
-var app = express();
+// var app = express();
 
-const route = require('./routes/route');
+// const route = require('./routes/route');
 
-mongoose.connect('mongodb://localhost:27017/MERN');
+// mongoose.connect('mongodb://localhost:27017/MERN');
 
-mongoose.connection.on('connected', () => {
+// mongoose.connection.on('connected', () => {
 
-    console.log('Sucessfully connected');
+//     console.log('Sucessfully connected');
 
-});
+// });
 
-mongoose.connection.on('error', (err)=>{
-    if(err){
-        console.log('Connection is unsuccessful!'+err);
-    }
-});
+// mongoose.connection.on('error', (err)=>{
+//     if(err){
+//         console.log('Connection is unsuccessful!'+err);
+//     }
+// });
 
-const port = 3000;
+// const port = 3000;
 
+
+// app.use(cors());
+
+// app.use(bodyparser.json())
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+
+//     res.send('Ri-Research-Lab--Tarafder-Informatics-Ltd--Techbology-Academy--IEC-R-Foundation');
+// });
+
+// app.use('/api', route);
+
+
+
+// app.listen(port, () => {
+//     console.log('Server started at port' + port)
+// });
+
+
+  
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const mysql = require('mysql2');
+const { send } = require('process');
+
+const app = express();
 
 app.use(cors());
+app.use(bodyparser.json());
 
-app.use(bodyparser.json())
 
-app.use(express.static(path.join(__dirname, 'public')));
+//database connection
 
-app.get('/', (req, res) => {
-
-    res.send('Ri-Research-Lab--Tarafder-Informatics-Ltd--Techbology-Academy--IEC-R-Foundation');
+const db = mysql.createConnection({
+    host: 'localhost',
+    videos: 'root',
+    password: '',
+    database: 'account',
+    port:7070
 });
 
-app.use('/api', route);
+//check database connection
+db.connect(err=>{
+    if(err) {console.log(err,'dberr');}
+    console.log('database connected ...')
+})
 
 
+//get all data
+app.get('/videos',(req,res)=>{
 
-app.listen(port, () => {
-    console.log('Server started at port' + port)
+    let qr = 'select * from videos';
+
+    db.query(qr,(err,result)=>{
+
+        if(err)
+        {
+            console.log(err,'errs');
+        }
+
+        if(result.length>0)
+        {
+            res.send({
+                message:'all videos data',
+                data:result
+            })
+        }
+    });
+});
+
+//get single data
+app.get('/videos/:id',(req,res)=>{
+
+    let gID = req.params.id;
+
+    let qr = `select * from videos where id = ${gID}`;
+
+    db.query(qr,(err,result)=>{
+
+        if(err) {console.log(err,'errs');}
+
+        if(result.length>0)
+        {
+            res.send({
+                message:'get single data',
+                data:result
+            })
+        }
+        else
+        {
+            res.send({
+                message:'data not found'
+            })
+        }
+    })
+})
+
+app.post('/videos',(req,res)=>{
+    console.log(req.body);
+});
+
+
+app.listen(3000,()=>{
+    console.log('server running......')
 });
