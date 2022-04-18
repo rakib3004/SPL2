02fd8dcdaa5e6f1e71ade7e3a58882e4369f9dc1
rakib3004 <bsse1129@iit.du.ac.bd>
2022-04-18 15:28:33 +0600

    add new angular template and imagine

--- BlogBee/src/app/blog-view/blog-view.component.ts
+++ BlogBee/src/app/blog-view/blog-view.component.ts
@@ -9,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class BlogViewComponent implements OnInit {
   constructor() {}
 
-  getBlog = JSON.parse('{"id": "KN5Bjh54uB8", "text": "Our environments are filled with information. ng study, his experimental design was revolutionary"}')
+  getBlog = JSON.parse('{"id": "KN5Bjh54uB8", "text": "Our environments are filled with information. ng study, his experimental design was revolutionary 21 years old to drink alcohol but only 18 Wall Street regulations, or Obamacare, or if there are any limits on when, where, and nd probably the most important, have you"}')
+  extraBlog = JSON.parse('{"id": "KN5Bjh54uB8", "text": "Our environments are filled with information. ng study, his experimental design was revolutionary 21 years old to drink alcohol but only 18 Wall Street regulations, or Obamacare, or if there are any limits on when, where, and nd probably the most important, have you"}')
 
   blogId=this.getBlog.id;
   blogText=this.getBlog.text;
