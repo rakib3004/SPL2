a3396385b1f05accb522fbeaaa017453975720c4
rakib3004 <bsse1129@iit.du.ac.bd>
2022-05-29 14:07:36 +0600

    re write show content

--- BlogBee/src/app/blog-view/blog-view.component.ts
+++ BlogBee/src/app/blog-view/blog-view.component.ts
@@ -9,11 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class BlogViewComponent implements OnInit {
   constructor() {}
 
- // getBlog = JSON.parse('Blog.json')
-  extraBlog = JSON.parse('{"id": "KN5Bjh54uB8", "text": "Our environments are filled with information. ng study, his experimental design was revolutionary 21 years old to drink alcohol but only 18 Wall Street regulations, or Obamacare, or if there are any limits on when, where, and nd probably the most important, have you"}')
+  getBlog = JSON.parse('Blog.json')
+  //extraBlog = JSON.parse('{"id": "KN5Bjh54uB8", "text": "Our environments are filled with information. ng study, his experimental design was revolutionary 21 years old to drink alcohol but only 18 Wall Street regulations, or Obamacare, or if there are any limits on when, where, and nd probably the most important, have you"}')
 
-  blogId=this.extraBlog.id;
-  blogText=this.extraBlog.text;
+  blogId=this.getBlog.id;
+  blogText=this.getBlog.text;
 
 
   ngOnInit(): void {}
