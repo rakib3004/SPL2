58957a3da423449e42374164ac22b327f514ed7b
rakib3004 <bsse1129@iit.du.ac.bd>
2022-06-01 08:55:34 +0600

    modify fav-list-ts  at backend

--- BlogBee/src/app/favourite-list/favourite-list.component.ts
+++ BlogBee/src/app/favourite-list/favourite-list.component.ts
@@ -11,19 +11,20 @@ import { VideoService } from '../video.service';
 export class FavouriteListComponent implements OnInit {
 
   constructor(private videoService: VideoService, private router: Router) {}
-  videos: Video[] = [];
-  favVideo: Video[]=[] ;
+  videos: any = [];
+  favVideo: any =[];
 
 
   ngOnInit(): void {
-    this.videos = this.videoService.getVideos();
-    this.favVideo[0]=this.videos[4];
-    this.favVideo[1]=this.videos[11];
+    this.videoService.getVideos().subscribe(data=>{
+      this.videos = data;
+    });
+    this.favVideo[0]=this.videos[0];
+    this.favVideo[1]=this.videos[1];
     this.favVideo[2]=this.videos[5];
     this.favVideo[3]=this.videos[3];
     this.favVideo[4]=this.videos[0];
     this.favVideo[5]=this.videos[9];
   }
  
-
 }
