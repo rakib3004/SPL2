c5903965eeb15edf65441d697fbc67ab3508f7eb
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2024-01-20 15:56:08 +0600

    resolve text classification issue

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -186,7 +186,7 @@ app.post('/tags',(req,res)=>{
     let outputData = '';
 
     const { spawn } = require('child_process');
-    const pyProg = spawn('python', ['./ClassifyText.py']);
+    const pyProg = spawn('python', ['./predictTextClassification.py']);
 
     pyProg.stdout.on('data', function(data) {
         outputData += data.toString();
--- BackEnd/predictTextClassification.py
+++ BackEnd/predictTextClassification.py
@@ -4,8 +4,6 @@ from sklearn.datasets import fetch_20newsgroups
 
 twenty_train = fetch_20newsgroups()
 
-print(twenty_train)
-
 loaded_model = joblib.load('text_classification_model.joblib')
 
 deployment_text = open("Text.txt", "r")
